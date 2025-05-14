39e5c3bf0ed58d9c81a3a08e0a8342349ca67302
Ashler Herrick <ashlerjherrick@gmail.com>
2025-05-14 09:14:30 -0400

    added postgres folder

---
+++ postgres/initdb/create_ingestion.sql
@@ -0,0 +1,23 @@
+CREATE SCHEMA IF NOT EXISTS ingestion;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS ingestion.raw_api_ingest (
+    object_id     UUID PRIMARY KEY,                  -- supplied by API
+    format          TEXT      NOT NULL,              -- e.g. 'fhir', 'hl7', 'note'
+    content_type    TEXT,                            -- e.g. 'json', 'xml', 'text'
+    subtype         TEXT      NOT NULL,              -- e.g. 'Patient', 'Observation', or generic type
+    data_version    TEXT,                            -- e.g. 'r4', 'stu3', version tag for non-FHIR types
+    storage_path    TEXT      NOT NULL,              -- URI or path to raw payload
+    fingerprint     BYTEA     NOT NULL UNIQUE,       -- ShA256 hash of the validated payload to confirm uniqueness
+    received_at     TIMESTAMPTZ DEFAULT now()        -- ingestion timestamp
+);
+
+
+CREATE TABLE IF NOT EXISTS ingestion.ingestion_log (
+    log_id          BIGSERIAL PRIMARY KEY,
+    object_id     UUID REFERENCES ingestion.raw_api_ingest(object_id),
+    log_time        TIMESTAMPTZ DEFAULT now(),       -- event timestamp
+    status          TEXT      NOT NULL,              -- e.g. 'duplicated', 'ingested', 'failed'
+    message         TEXT                             -- error or info message
+);
